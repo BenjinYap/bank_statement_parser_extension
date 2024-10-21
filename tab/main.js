4731818a45dc5b3aa6741015c7c53c271c26c5d8
Benjin Yap <benjyap7+github@gmail.com>
2024-10-20 21:40:12 -0400

    Delete popup.

--- tab/main.js
+++ tab/main.js
@@ -1,83 +1,6 @@
+import { CATEGORY_REPLACEMENTS, doReplacement, ITEM_REPLACEMENTS } from "./js/replacements.mjs";
 
-const CATEGORY_REPLACEMENTS = {
-  'Food': [
-    'drinks to go',
-    'drinks for home',
-    'eating out',
-    'groceries',
-    "haniya's fridge",
-    'snacks',
-  ],
-  'Bills': [
-    'amazon web services',
-    'phone',
-    'internet',
-  ],
-  'Subscriptions': [
-    'spotify',
-    'jetbrains',
-    'netflix',
-  ],
-  'Eunice': [
-    'Food',
-  ],
-  'Haniya': [
-    'Adventures',
-  ],
-  'Transportation': [
-    'fuel',
-  ],
-};
-const ITEM_REPLACEMENTS = {
-  'Eating out': [
-    'noodlebox',
-    'sams grill',
-    'WOK BOX',
-    'lazeez',
-    'fluffy glaze',
-    'kibo market union',
-  ],
-  'Eating out Haniya': [
-    'mcdonald',
-    'wendys',
-    'marble slab',
-    'a&w',
-    'popeyes',
-    'east side marios',
-  ],
-  'Drinks to go': [
-    ['tim hortons', (d, i, a) => a === 3.14],
-    'coco fresh tea',
-    'chatime',
-    'gotcha bubble tea',
-    'gongcha',
-    'gong cha',
-    ['centurion coffee', (d, i, a) => a === 5.59],
-    ['esso circle', (d, i, a) => a === 2.51],
-  ],
-  'Coffee shop': [
-    'java beans',
-  ],
-  'Internet': ['bell canada'],
-  'Phone': ['virgin plus'],
-  'Balance Protection Tax': ['Balance Protection Tax'],
-  'Balance Protection Insurance': ['Balance Protection Ins'],
-  'Spotify': ['Spotify'],
-  'Netflix': ['netflix'],
-  'JetBrains': ['jetbrains'],
-  'Food': ['sp royal canin'],
-  'Adventures': [
-    'the round table',
-    'activate cambridge',
-    'exitus escape room',
-    'sky zone',
-    'claw me baby',
-  ],
-  'Fuel': [
-    'mobil@',
-  ],
-};
-
+// noinspection JSUnresolvedReference,JSDeprecatedSymbols
 chrome.runtime.onMessage.addListener((request) => {
   if (request.action === 'tab_opened') {
     work(request);
@@ -246,28 +169,7 @@ function buildSuccessRow(date, item_raw, amount) {
   return obj;
 }
 
-function doReplacement(date, item_raw, amount, replacements) {
-  for (const item_replace in replacements) {
-    for (const condition of replacements[item_replace]) {
-      // condition is just a regex string
-      if (typeof condition === 'string') {
-        const re = new RegExp(`.*${condition}.*`, 'i');
-        if (item_raw.match(re)) {
-          return item_replace;
-        }
-      } else {  // condition is a regex string plus a custom filter function
-        const re = new RegExp(`.*${condition[0]}.*`, 'i');
 
-        const filter = condition[1];
-        if (filter(date, item_raw, amount) && item_raw.match(re)) {
-          return item_replace;
-        }
-      }
-    }
-  }
-
-  return null;
-}
 
 function getFormattedDate(date) {
   let year = date.getFullYear();
